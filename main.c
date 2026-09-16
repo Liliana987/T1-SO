ab197a1cb99a2be9fdec294f0b48795da7e08931
liliana987 <liliana.albornoz@mail.udp.cl>
2026-09-15 23:33:24 -0300

    a

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
 
 int main(void) {
     printf("Hola, planificador\n");
